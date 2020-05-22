955f8895709d68281975a9bb56690f454996e619
Julio Cesar da Silva Pereira <thisjulio@gmail.com>
2020-05-22 14:42:22 -0300

    Initial Commit

---
+++ Treinamento-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Treinamento-ios
+//
+//  Created by Julio Cesar da Silva Pereira on 22/05/2020.
+//  Copyright © 2020 Julio Cesar da Silva Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
